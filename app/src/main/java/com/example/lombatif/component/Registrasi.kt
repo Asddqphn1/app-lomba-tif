405b9490814b6769b281d9e6ad040c9a5895adde
Dandy Teguh Pratama <dandypratama0987@gmail.com>
2025-05-14 21:14:41 +0700

    Register Commit

--- app/src/main/java/com/example/lombatif/component/Registrasi.kt
+++ app/src/main/java/com/example/lombatif/component/Registrasi.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,13 +17,17 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -39,6 +44,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.withStyle
@@ -46,72 +52,72 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.lombatif.R
+import com.example.lombatif.component.ui.theme.LombaTIFTheme
 
-import com.example.lombatif.ui.theme.LombaTIFTheme
-
-
-class MainActivity : ComponentActivity() {
+class Registrasi : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             LombaTIFTheme {
-                Login()
+               Register()
             }
         }
     }
 }
 
 @Composable
-fun Login(){
+fun Register(){
+    var name by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+    var checked by remember { mutableStateOf(false) }
     val context = LocalContext.current
-    Box (modifier = Modifier.fillMaxSize()) {
-        Column (modifier = Modifier
+    Box(modifier = Modifier.fillMaxSize()){
+        Column(modifier = Modifier
             .padding(16.dp)
             .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally) {
-
-            Spacer(modifier = Modifier.height(60.dp))
-
-            Image(
-                painter = painterResource(id = R.drawable.logotiflomba),
-                contentDescription = "logoAPP",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(120.dp)
-                    .clip(CircleShape)
-
-            )
-
+            horizontalAlignment = Alignment.CenterHorizontally)
+        {
             Spacer(modifier = Modifier.height(30.dp))
+
             Text(
                 text = "LombaTIF",
                 fontWeight = FontWeight.Bold,
                 fontSize = 30.sp,
                 fontFamily = FontFamily.Serif
             )
-            Spacer(modifier = Modifier.height(6.dp))
+
+            Spacer(modifier = Modifier.height(25.dp))
+
+           Image(
+               painter = painterResource(id = R.drawable.logotiflomba),
+               contentDescription = "logoLomba",
+               contentScale = ContentScale.Crop,
+               modifier = Modifier
+                   .clip(CircleShape)
+                   .size(120.dp)
+           )
+
+            Spacer(modifier = Modifier.height(25.dp))
+
+            Text(
+                text = "Register",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                fontFamily = FontFamily.Serif
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
             Text(
                 text = "Competition Management Platfrom",
                 fontSize = 15.sp,
                 color = Color.DarkGray,
                 fontWeight = FontWeight.Medium
             )
-            Spacer(modifier = Modifier.height(40.dp))
-            Text(
-                text = "Welcome Back",
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-            )
-            Spacer(modifier = Modifier.height(6.dp))
-            Text(
-                text = "Please Login to Your Account",
-                fontSize = 15.sp,
-                color = Color.DarkGray,
-                fontWeight = FontWeight.Medium
-            )
+
             Spacer(modifier = Modifier.height(10.dp))
 
             Column(
@@ -120,7 +126,26 @@ fun Login(){
                     .padding(24.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                // EMAIL
+                // Username
+                OutlinedTextField(
+                    value = name,
+                    onValueChange = { name = it },
+                    label = { Text("Username") },
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Default.Person, contentDescription = null)
+                    },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color(0xFF1C3ED3),
+                        focusedTextColor = Color.Black,
+                        focusedPlaceholderColor = Color(0xFF000000)
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                // Email
                 OutlinedTextField(
                     value = email,
                     onValueChange = { email = it },
@@ -128,8 +153,10 @@ fun Login(){
                     leadingIcon = {
                         Icon(imageVector = Icons.Default.Email, contentDescription = null)
                     },
+                    visualTransformation = PasswordVisualTransformation(),
                     singleLine = true,
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxWidth(),
                     colors = OutlinedTextFieldDefaults.colors(
                         focusedBorderColor = Color(0xFF1C3ED3),
                         focusedTextColor = Color.Black,
@@ -158,6 +185,56 @@ fun Login(){
                     )
                 )
 
+                Spacer(modifier = Modifier.height(12.dp))
+
+                // CONFIRM PASSWORD
+                OutlinedTextField(
+                    value = confirmPassword,
+                    onValueChange = { confirmPassword = it },
+                    label = { Text("Confirm Password") },
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Default.Check, contentDescription = null)
+                    },
+                    visualTransformation = PasswordVisualTransformation(),
+                    singleLine = true,
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color(0xFF1C3ED3),
+                        focusedTextColor = Color.Black,
+                        focusedPlaceholderColor = Color(0xFF000000)
+                    )
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 0.dp)
+                ) {
+                    Checkbox(
+                        checked = checked,
+                        onCheckedChange = { checked = it }
+                    )
+                    Text(
+                        buildAnnotatedString {
+                            append("I agree to the  ")
+
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.Blue,
+                                    fontWeight = FontWeight.Medium
+                                )
+                            ) {
+                                append("Terms and Conditions and Privacy Policy")
+                            }
+                        },
+                        fontSize = 15.sp,
+                        color = Color.Gray,
+                        fontWeight = FontWeight.Medium
+                    )
+                }
                 Spacer(modifier = Modifier.height(25.dp))
 
                 Button(
@@ -169,17 +246,18 @@ fun Login(){
                     shape = RoundedCornerShape(8.dp)
                 ) {
                     Text(
-                        text = "Login",
+                        text = "Register",
                         fontWeight = FontWeight.SemiBold,
                         fontSize = 17.sp,
                         color = Color.White
                     )
                 }
+
                 Spacer(modifier = Modifier.height(25.dp))
 
                 Text(
                     buildAnnotatedString {
-                        append("Don't have an account yet? ")
+                        append("Already have an account? ")
 
                         withStyle(
                             style = SpanStyle(
@@ -187,7 +265,7 @@ fun Login(){
                                 fontWeight = FontWeight.Bold
                             )
                         ) {
-                            append("Sign Up")
+                            append("Login")
                         }
                     },
                     fontSize = 15.sp,
@@ -200,12 +278,8 @@ fun Login(){
     }
 }
 
-
 @Preview(showBackground = true)
 @Composable
-fun ShowLogin(){
-    Login()
+fun ShowRegister() {
+    Register()
 }
-
-
-
